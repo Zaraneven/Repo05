a3e435d4cca45527312951b7af47d57c7301775a
neven kusturica <neven.kusturica@gmail.com>
2022-04-01 21:16:08 +0200

    Added ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+
+    var myFirstName = "Neven";
+    var myLastname = "Kusturica";
+    var myHomeTown = "Sarajevo";
+
+    return {
+        myFirstName,
+        myLastname,
+        myHomeTown
+    }
+}
+console.log(main());
+module.exports = main;
