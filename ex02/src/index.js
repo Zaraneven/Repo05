2a5f3da69324147c0ab21b6ceb7d09e807843c4f
neven kusturica <neven.kusturica@gmail.com>
2022-04-01 22:32:25 +0200

    Added ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+
+    myFirstString = "I think therefore I \"am\" -- sentient.";
+    mySecondString = "No, you only \"think\", so you aren't.";
+
+    return {
+        myFirstString,
+        mySecondString
+    };
+}
+console.log(main());
+module.exports = main;
